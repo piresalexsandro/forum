92376a1bf2dc716d02912b8268b0ad92d565a320
piresalexsandro <allexsandro.pires@gmail.com>
2020-09-07 20:52:49 -0300

    Documentando Api com Swagger

--- src/main/java/br/com/alphapires/forum/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/alphapires/forum/config/security/SecurityConfiguration.java
@@ -72,7 +72,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	// Configurações de recurso estatico(arquivos css, js, imagens, etc...)
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-
+		web.ignoring()
+        .antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
 	}
 
 //	public static void main(String[] args) {
